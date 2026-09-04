358ddd3817639525e030b8dacc554963cc4b3421
Jafa Yassine <jafayassine03@gmail.com>
2026-09-04 16:41:20 +0100

    Update calcul2.c

--- calcul2.c
+++ calcul2.c
@@ -22,7 +22,10 @@ int main() {
         printf("6. Power\n");
         printf("7. Square Root\n");
         printf("8. Factorial\n");
-        printf("9. Exit\n");
+        printf("9. Sine\n");
+        printf("10. Cosine\n");
+        printf("11. Tangent\n");
+        printf("12. Exit\n");
         printf("Choose an option: ");
         scanf("%d", &choice);
 
@@ -44,4 +47,72 @@ int main() {
                 break;
             case 4:
                 printf("Enter two numbers: ");
-                scanf("%lf %lf",
+                scanf("%lf %lf", &num1, &num2);
+                if (num2 == 0) {
+                    printf("Error: Division by zero!\n");
+                } else {
+                    printf("Result: %.2lf\n", num1 / num2);
+                }
+                break;
+            case 5: {
+                int a, b;
+                printf("Enter two integers: ");
+                scanf("%d %d", &a, &b);
+                if (b == 0) {
+                    printf("Error: Division by zero!\n");
+                } else {
+                    printf("Result: %d\n", a % b);
+                }
+                break;
+            }
+            case 6:
+                printf("Enter base and exponent: ");
+                scanf("%lf %lf", &num1, &num2);
+                printf("Result: %.2lf\n", pow(num1, num2));
+                break;
+            case 7:
+                printf("Enter a number: ");
+                scanf("%lf", &num1);
+                if (num1 < 0) {
+                    printf("Error: Negative number!\n");
+                } else {
+                    printf("Result: %.2lf\n", sqrt(num1));
+                }
+                break;
+            case 8: {
+                int n;
+                printf("Enter an integer: ");
+                scanf("%d", &n);
+                long long result = factorial(n);
+                if (result == -1) {
+                    printf("Error: Factorial of negative number!\n");
+                } else {
+                    printf("Result: %lld\n", result);
+                }
+                break;
+            }
+            case 9:
+                printf("Enter angle in degrees: ");
+                scanf("%lf", &num1);
+                printf("Result: %.4lf\n", sin(num1 * M_PI / 180));
+                break;
+            case 10:
+                printf("Enter angle in degrees: ");
+                scanf("%lf", &num1);
+                printf("Result: %.4lf\n", cos(num1 * M_PI / 180));
+                break;
+            case 11:
+                printf("Enter angle in degrees: ");
+                scanf("%lf", &num1);
+                printf("Result: %.4lf\n", tan(num1 * M_PI / 180));
+                break;
+            case 12:
+                printf("Exiting calculator. Goodbye!\n");
+                break;
+            default:
+                printf("Invalid choice. Try again.\n");
+        }
+    } while (choice != 12);
+
+    return 0;
+}
